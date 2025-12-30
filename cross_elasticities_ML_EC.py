3e279dee3ec8adcc5231acee69ff8f7518832561
villeisoranta99 <ville.isoranta99@gmail.com>
2025-12-30 15:39:01 +0200

    Update cross_elasticities_ML_EC.py

--- cross_elasticities_ML_EC.py
+++ cross_elasticities_ML_EC.py
@@ -1,6 +1,7 @@
 """
 Calculation of cross elasticities for the PML model
 Ville Isoranta 19.11.2025
+Results shown in Sections 4.2.3 and 4.2.4
 """
 """
 Imports necessary expressions.
@@ -294,5 +295,6 @@ print(
     f'Aggregate cross point elasticity of garage wrt search time of on-street parking: '
     f'{cross_elas_term_SS_G:.3g}'
 )
+
 
 
